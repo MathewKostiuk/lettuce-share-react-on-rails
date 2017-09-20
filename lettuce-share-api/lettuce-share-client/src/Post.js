b3a2cb3300609cc0f2df7e11662cb77656e30216
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-09-20 06:48:03 +0000

    implemented dummy Post component

---
+++ lettuce-share-api/lettuce-share-client/src/Post.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+
+class Post extends Component {
+
+  render() {
+    return (
+      <div>
+        <span>This is a test post</span>
+      </div>
+    )
+  }
+}
+
+export default Post;
