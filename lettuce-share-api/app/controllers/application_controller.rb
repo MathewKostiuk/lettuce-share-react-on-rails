d443a0b52da3406bbd7c58bbf485b9d7aef63511
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-10-04 22:38:51 +0000

    implemented JWT auth methods in back-end

--- lettuce-share-api/app/controllers/application_controller.rb
+++ lettuce-share-api/app/controllers/application_controller.rb
@@ -3,5 +3,43 @@ require "application_responder"
 class ApplicationController < ActionController::API
   self.responder = ApplicationResponder
   respond_to :html
+  before_action :authenticate
+
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    if auth_present?
+      user = User.find(auth["user"])
+      if user
+        @current_user ||= user
+      end
+    end
+  end
+
+  def authenticate
+    unless logged_in?
+      render json: {error: 'unauthorized'}, status: 401
+    end
+  end
+
+  private
+
+  def token
+    request.env['HTTP_AUTHORIZATION'].scan(/Bearer
+      (.*)$/).flatten.last
+  end
+
+  def auth
+    Auth.decode(token)
+  end
+
+  def auth_present?
+    !!request.env.fetch("HTTP_AUTHORIZATION",
+      "").scan(/Bearer/).flatten.first
+  end
+
 
 end
--- lettuce-share-api/config/routes.rb
+++ lettuce-share-api/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
     resources :posts
+    post '/login', to: 'sessions#create'
   end
 end
