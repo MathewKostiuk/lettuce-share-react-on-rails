073ab83f3e2e89fb2ffe1875fb108e277a1e1775
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-10-11 23:15:23 +0000

    implemented nav bar with flex box

--- lettuce-share-api/lettuce-share-client/src/Components/Navigation.js
+++ lettuce-share-api/lettuce-share-client/src/Components/Navigation.js
@@ -6,11 +6,11 @@ class Navigation extends Component {
   render() {
     return (
       <header>
-        <nav className='navigation'>
-          <ul>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/posts'>Home</Link></li>
-            <li><Link to='/register'>Register</Link></li>
+        <nav>
+          <ul className='navigation'>
+            <li><Link to='/login' className='nav-link'>Login</Link></li>
+            <li><Link to='/posts' className='nav-link'>Home</Link></li>
+            <li><Link to='/register' className='nav-link'>Register</Link></li>
           </ul>
         </nav>
       </header>
